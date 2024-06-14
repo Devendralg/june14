775a4973793df94561302271cb6dd4b937951c5d
Devendralg <devendralg18@gmail.com>
2024-06-14 12:56:10 +0530

    June14

---
+++ ff/src/test/java/jenkin/ATest.java
@@ -0,0 +1,24 @@
+package jenkin;
+
+import org.testng.annotations.Test;
+
+public class ATest {
+
+	@Test
+	public void a()
+	{
+		System.out.println("a");
+	}
+	@Test
+	public void b()
+	{
+		System.out.println("b");
+
+	}
+	@Test
+	public void c()
+	{
+		System.out.println("b");
+
+	}
+}
---
+++ ff/src/test/java/jenkin/BTest.java
@@ -0,0 +1,24 @@
+package jenkin;
+
+import org.testng.annotations.Test;
+
+public class BTest {
+
+	@Test
+	public void a()
+	{
+		System.out.println("a");
+	}
+	@Test
+	public void b()
+	{
+		System.out.println("b");
+
+	}
+	@Test
+	public void c()
+	{
+		System.out.println("b");
+
+	}
+}
